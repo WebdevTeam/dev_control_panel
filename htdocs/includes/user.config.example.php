da33f368ca9fbd43409156c1f88419b89994dd65
Usman Niazi <usmanniazi786@gmail.com>
2014-06-27 14:13:14 +0100

    [UPDATE] Added more information about password Limbo uses to login users

--- htdocs/includes/user.config.example.php
+++ htdocs/includes/user.config.example.php
@@ -1,3 +1,3 @@
 <?php
 $sub_user = "Your LIMBO username";
-$sub_pass = "Password LIMBO uses to log you into live sites";
+$sub_pass = "Password LIMBO uses to log you into live sites which at the moment is 'f4d3_2_bl4ck' for every user";
