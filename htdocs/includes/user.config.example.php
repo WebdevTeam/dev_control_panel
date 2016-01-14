9f81e0af16dab813135f10990b1e35246385bdc2
Usman Niazi <usman.niazi@worldstores.co.uk>
2016-01-14 08:32:08 +0000

    [UPDATE] Replaced TRAC with Gemini. Can now send search terms / ticket number to gemini. Pressing 'CTRL' + 'N' Opens new window and takes to the Board (as specified in your config)

--- htdocs/includes/user.config.example.php
+++ htdocs/includes/user.config.example.php
@@ -1,8 +1,13 @@
 <?php
 
 //This is you user configuration file.
-//Please place next to this file and call the file "user.config.php"
+//Using this as a template, create file "user.config.php" in this folder
 //Replace the below values with your own settings
 
 $sub_user = "Your LIMBO username";
 $sub_pass = "Password LIMBO uses to log you into live sites which at the moment is 'f4d3_2_bl4ck' for every user";
+
+$gemini = array(
+		"board_id" => "1564",
+		"board_name" => "OPSU"
+);
--- htdocs/js/status.js
+++ htdocs/js/status.js
@@ -76,24 +76,20 @@ $(document).ready(function()
 
 	$("#go").click(function()
 	{
-		var ticket=$("#ticket").val();
+		var gemini_search=$("#gemini_search").val();
 
-		//Check if the ticket number starts with #. (if it does, substring  and slice would be same)
-		if(ticket.slice(0,1) == '#')
-		{
-			ticket = ticket.slice(1);
-		}
+		var url = "http://supportdesk.it.worldstores.co.uk/project/0/items/?search=" + gemini_search;
 
 		//If entered value is int, assume it to be a ticket number
-		if(Math.floor(ticket) == ticket && $.isNumeric(ticket))
-		{
-			var url = "http://trac.worldstores.co.uk/ticket/" + ticket;
-		}
-		// Else assume, searching track
-		else
-		{
-			var url = "http://trac.worldstores.co.uk/search?q=" + ticket;
-		}
+		// if(Math.floor(ticket) == ticket && $.isNumeric(ticket))
+		// {
+		// 	var url = "supportdesk.it.worldstores.co.uk/project/0/items/?search=" + ticket;
+		// }
+		// // Else assume, searching track
+		// else
+		// {
+		// 	var url = "http://trac.worldstores.co.uk/search?q=" + ticket;
+		// }
 
 		var win = window.open(url, '_blank');
 		win.focus();
@@ -106,7 +102,7 @@ $(document).ready(function()
 		$("#"+panel).fadeToggle();
 	});
 
-	$("#ticket").keypress(function(event)
+	$("#gemini_search").keypress(function(event)
 	{
 	    if(event.keyCode == 13)
 	    {
@@ -122,12 +118,12 @@ $(document).ready(function()
 			//key 68 = 'D' - Select ticket input field
 			if (e.keyCode == "68" && e.ctrlKey == true)
 			{
-				$("#ticket").select();
+				$("#gemini_search").select();
 			}
 			//key 78 = 'N' - Create new ticket
 			if (e.keyCode == "78" && e.ctrlKey == true)
 			{
-				var url = "http://trac.worldstores.co.uk/newticket";
+				var url = "http://supportdesk.it.worldstores.co.uk/workspace/1564/board";
 				var win = window.open(url, '_blank');
        			win.focus();
 			}
